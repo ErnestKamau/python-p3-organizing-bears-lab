115ad4435a3c61a0144d93583b76a2b4a2d6df42
Ernest Kamau <1.kamauernest@gmail.com>
2025-05-20 11:04:50 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "Brown", "Melancholic", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 7, "M", "Brown-black", "Fiesty", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 20, "F", "Black", "Friendly", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 10, "M", "Brown", "Fiesty", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, "F", "Black and White", "Cheerful", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 4, "M", "White", "Sensitive", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 15, "F", "Brown", "Melancholic", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, "M", "Black", "Reserved", 1);
